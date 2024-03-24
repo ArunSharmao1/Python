a904f37dc4fcf2a5840308af29ed4e9398ad4360
ArunSharmao1 <164701620+ArunSharmao1@users.noreply.github.com>
2024-03-24 06:40:16 +0530

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,7 @@
+#collection of data will be stored and store any type of data. Stores in []
+l1=[1,2,"Arun",True,34,65,]
+print(type(l1))#for check the type of data
+print(l1[:-1])#for reverse the list
+l1[2]="Arun Sharma"#change the data permanentaly
+print(l1)#print the changed data
+
